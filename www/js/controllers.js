ea8d6d50f8d3d54d1c756c8a6dd95f6cfb248297
Steve <stevenlyall@gmail.com>
2015-01-27 18:52:01 -0800

    testing service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,11 @@
-var ernieApp= angular.module('ernieApp', []);
+var ernieApp = angular.module('ernieApp', []);
 
 
 // controller
-ernieApp.controller('surveyController', function surveyController($scope) {
-    $scope.questions = [
-        {
-            "id" : "q1",
-            "question" : "At the moment I feel happy",
-            "scale" : "5",
-            "response" : "0"
-        },
-        {
-            "id" : "q2",
-            "question" : "At the moment I feel relaxed",
-            "scale" : "5",
-            "response" : "0"
-        },
-        {
-            "id" : "q2",
-            "question" : "At the moment I feel angry",
-            "scale" : "5",
-            "response" : "0"
-        },
-        {
-            "id" : "q3",
-            "question" : "At the moment I feel stressed",
-            "scale" : "5",
-            "response" : "0"
-        },
-        {
-            "id" : "q4",
-            "question" : "At the moment I feel anxious",
-            "scale" : "5",
-            "response" : "0"
-        },
-        {
-            "id" : "q5",
-            "question" : "At the moment I feel depressed",
-            "scale" : "5",
-            "response" : "0"
-        }
-    ]
-
-
-
+ernieApp.controller('surveyController', function surveyController($scope, $http) {
+    // use http servie to get data from json file
+    $http.get('js/questions.json').success(function(data) {
+        $scope.questions = data;
+    });
 
 });
