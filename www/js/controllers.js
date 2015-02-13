c893678e03e0b6697cd06bc175ba34c38c5ebaee
Steve <stevenlyall@gmail.com>
2015-02-13 12:12:21 -0800

    paging through list of questions

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,26 @@ angular.module('ernie-app.controllers',[])
 
     .controller('surveyCtrl', function ($scope, $http) {
 
-        $scope.questions;
+        // current question number
+        var questionNum = 0;
+
+        // holds current question data
+        $scope.currentQuestion;
+
+        // index of selected response
+        $scope.selectedResponse = -1;
 
         // create model for questions
         // use http service to get data from json file
         $http.get('js/questions.json').success(function(data) {
             $scope.questions = data.items;
-            $scope.currentQuestion = $scope.questions[0];
+            $scope.getQuestion(questionNum);
         })
+
+        // get a new question at sepcified index from the question list
+        $scope.getQuestion = function(index) {
+            $scope.currentQuestion = $scope.questions[index];
+        }
 
         // get response button elements
         var buttons = [];
@@ -18,16 +30,27 @@ angular.module('ernie-app.controllers',[])
             buttons[i] = button;
         }
 
-        // index of selected resposne
-        $scope.selectedResponse = -1;
 
         // handler for next button
         $scope.nextClick = function() {
             console.log("next Button Clicked");
-            console.log($scope.questions.toString());
-            for (var i = 0; i<($scope.questions.length); i++) {
-                console.log(i);
+
+            // save response
+            // $scope.currentQuestion.response = selectedResponse;
+            console.log("response saved " + $scope.currentQuestion.response);
+
+            // advance to next question
+            $scope.selectedResponse = -1;
+            questionNum++;
+            $scope.getQuestion(questionNum);
+
+            // clear selections
+            for (var i in buttons) {
+                buttons[i].className = "itembutton button";
             }
+
+            // hide next button
+            document.getElementById("nextButton").style.display = "none";
         }
 
         $scope.numSelect = function numSelect(num) {
@@ -41,6 +64,7 @@ angular.module('ernie-app.controllers',[])
             buttonPushed.className += (" selecteditembutton");
             selectedResponse = num;
 
+            // make next button visible
             document.getElementById("nextButton").style.display = "";
 
         }
