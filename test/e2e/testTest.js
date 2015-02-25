35578563e2e023dfa3178ec27c3521aec324cf1c
Steve <stevenlyall@gmail.com>
2015-02-25 15:14:37 -0800

    protractor added 1

---
+++ test/e2e/testTest.js
@@ -0,0 +1,6 @@
+// Describe a feature
+describe('Becoming Awesome', function(){
+    it('should start out not very awesome', function() {
+        expect($scope.questionIndex).toEqual(0);
+    });
+});
