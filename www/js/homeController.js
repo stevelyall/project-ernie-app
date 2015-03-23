9539bca8b8f97b9cf69c74e2aa01de2c034e8728
Steve <stevenlyall@gmail.com>
2015-03-23 15:10:02 -0700

    removed redundant email plugin, add ngCordova

--- www/js/homeController.js
+++ www/js/homeController.js
@@ -1,6 +1,13 @@
 angular.module('ernie-app.controllers')
     .controller('homeController', function($scope, $state, $ionicPopup) {
 
+        //document.addEventListener("deviceready", onDeviceReady, false);
+        //function onDeviceReady() {
+        //    console.log(device.cordova);
+        //    console.log(device.model);
+        //    console.log(device.name);
+        //    console.log(device.uuid);
+        //}
 
         $scope.startSurveyButtonOnClick = function() {
             console.log("startsurveybuttononclick");
