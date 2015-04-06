cb6287f3451691acfe6b27859ada6a54b5584497
Steve <stevenlyall@gmail.com>
2015-04-06 14:34:21 -0700

    ion-pane plays nicer with iOs. also fixes #16

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,7 @@ var app = angular.module('ernie-app', ['ionic', 'ernie-app.controllers', 'ngCord
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
